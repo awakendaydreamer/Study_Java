3953db485d6d365c2097eae5c40f9c9ebff77ba5
park <neometin21@naver.com>
2015-08-20 16:42:42 +0900

    블럭 if 테스트

---
+++ src/If/Test_if_else.java
@@ -0,0 +1,17 @@
+package If;
+public class Test_if_else {
+	public static void main(String[] args) {
+		
+/* --------------------------------------블럭 if ----------------------*/
+		//성별코드(code)값이 1 또는 3이면 '남자',그외에는 '여자'
+		
+		int code = 3;
+		if((code == 1) || (code == 3)){
+			System.out.println("남자입니다.");
+		} else{
+			System.out.println("여자입니다.");
+		}//End of if
+
+	
+	}//End of main()
+}//End of class
