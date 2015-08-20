3ab058e13a90b4f290915d4a0290a491c504034e
park <neometin21@naver.com>
2015-08-20 15:01:39 +0900

    8진수와 16진수

---
+++ src/Operator/Operator09.java
@@ -0,0 +1,21 @@
+package Operator;
+public class Operator09 {
+	public static void main(String[] args) {
+
+		//16진수 : 숫자 앞에 '0x'를 붙인다.
+		//8진수  : 숫자 앞에 '0'을 붙인다.
+		
+		int a = 11;
+		System.out.println("10진수의 11값 :" + "\t" + a); //10진수
+		
+		int b = 0x11;
+		System.out.println("16진수의 11값 :" + "\t" + b); //16진수
+		
+		int c = 011;
+		System.out.println("8진수의 11값 :" + "\n" + c);  //8진수
+	
+		
+		
+	}//End of main()
+
+}//End of Class
