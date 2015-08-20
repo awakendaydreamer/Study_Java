addc70514e1b3310e2aff8160176ec49b01e24ee
park <neometin21@naver.com>
2015-08-20 16:27:16 +0900

    블럭 if메소드

---
+++ src/If/Ex_if_else.java
@@ -0,0 +1,25 @@
+package If;
+public class Ex_if_else {
+	public static void main(String[] args) {
+
+		
+/*------------------------------------블럭 if--------------------------*/
+	/*블럭 if문 : 조건을 판단하여 참과 거짓일때의 명령을 실행
+	 * 
+	 * if(조건식){
+	 * 	참일때 실행되는 문장;
+	 * } else{
+	 * 	거짓일때 실행되는 문장;
+	 * }	
+	 */
+		int a = 10;
+		if(a % 2 == 0){
+			System.out.println("짝수입니다.");		//조건식이 true
+		} else {
+			System.out.println("짝수입니다.");		//조건식이 false
+		}
+		
+		
+	}
+
+}
