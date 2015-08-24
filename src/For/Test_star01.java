0c794c5f633076431a93db7c25395796e3da6614
park <neometin21@naver.com>
2015-08-24 12:33:15 +0900

    Test_star01

---
+++ src/For/Test_star01.java
@@ -0,0 +1,38 @@
+package For;
+
+public class Test_star01 {
+	public static void main(String[] args) {
+	/*별찍기	
+		□□□□★
+		□□□★★
+		□□★★★
+		□★★★★
+		★★★★★
+	*/	
+		
+		
+/*		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 6-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}//int
+*/		
+		
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 5; j >i ; j--) {
+//				System.out.print("□");
+//			}//for j
+//			for (int k = 1; k <= i; k++) {
+//				System.out.print("★");
+//			}//for k
+//			System.out.println();
+//		}//for i
+		
+
+		
+		
+		
+		
+	}//main
+}// class
