bb48406c24e8f152e3ede89b57ccfc9567e1f88c
park <neometin21@naver.com>
2015-08-24 11:32:35 +0900

    double_for01

---
+++ src/For/Ex_doublefor01.java
@@ -0,0 +1,15 @@
+package For;
+public class Ex_doublefor01 {
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=5 ; i++) { //int i :1부터 5까지 반복
+			for (int j = 1; j <= i; j++) { //int j :1부터 i까지 반복
+				System.out.println("i 값: " + i + "\t" + "j 값 :" + j);
+			}//for j
+		
+			
+		}//for i
+		
+	}//main()
+
+}//class
